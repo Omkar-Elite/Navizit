4ddae8aad036c3a04b7a3e7d182d39fa072c82fb
Omkar-Elite <143245257+Omkar-Elite@users.noreply.github.com>
2024-09-29 11:43:02 +0530

    Update server.js

--- src/Tourist/server.js
+++ src/Tourist/server.js
@@ -7,8 +7,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;// Use the PORT environment variable
-
+const port = process.env.PORT || 3001;// Use the PORT environment variable
+const host = '0.0.0.0';
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -130,6 +130,6 @@ app.post("/api/login", (req, res) => {
   );
 });
 
-app.listen(port,   () => { 
-  console.log(`Server is running on http://0.0.0.0:${port}`);
+app.listen(port, host,   () => { 
+  console.log(`Server is running on http://${host}:${port}`);
 });
