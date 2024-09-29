9babca404ea580df1107ba5c54cbd3c611298e3a
Omkar-Elite <143245257+Omkar-Elite@users.noreply.github.com>
2024-09-29 13:45:50 +0530

    Update Logo.jsx

--- src/HomePage/Navbar/Logo.jsx
+++ src/HomePage/Navbar/Logo.jsx
@@ -1,10 +1,11 @@
 
+import logoIcon from './logomain.png';
 export default function Logo() {
 
     return (
         <>
             <div className="">
-                <img src="/src/HomePage/Navbar/logomain.png" alt="Logo"  className="w-[170px] h-[40px] ml-3 mix-blend-multiply bg-transparent"/>
+                <img src={logoIcon} alt="Logo"  className="w-[170px] h-[40px] ml-3 mix-blend-multiply bg-transparent"/>
             </div>
         </>
     )
